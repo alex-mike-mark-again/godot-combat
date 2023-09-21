f8ddbad4227796f23dc346c96ef7005705479108
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2023-09-20 23:42:26 -0500

    removes reference to local machine, mkdir mks the dir before it's used

--- ci/setup.sh
+++ ci/setup.sh
@@ -15,6 +15,6 @@ unzip godot.zip
 mv Godot_v4.1.1-stable_linux.x86_64 godot
 
 # set up templates
-unzip templates.zip -d /home/alex/.local/share/godot/export_templates
 mkdir ./.local/share/godot/export_templates
+unzip templates.zip -d ~/.local/share/godot/export_templates
 mv templates ./.local/share/godot/export_templates/4.1.1.stable
