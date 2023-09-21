100796878bd70fafb568d83ba07228ed4d188435
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2023-09-21 00:11:44 -0500

    need to download godot goof

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,7 +3,7 @@ cd ~
 
 # all the downloads in the universe
 curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
-# curl -L -o godot.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_linux.x86_64.zip
+curl -L -o godot.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_linux.x86_64.zip
 # curl -L -o templates.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_export_templates.tpz
 
 # set up butler
@@ -11,8 +11,8 @@ unzip butler.zip
 chmod +x butler
 
 # set up godot
-# unzip godot.zip
-# mv Godot_v4.1.1-stable_linux.x86_64 godot
+unzip godot.zip
+mv Godot_v4.1.1-stable_linux.x86_64 godot
 
 # set up templates
 # mkdir -p ~/.local/share/godot/export_templates
