47a52c506fc19f2a7f5040557661f4cd3814afa1
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2023-09-21 00:16:12 -0500

    templates again. added a slash this time

--- ci/setup.sh
+++ ci/setup.sh
@@ -4,7 +4,7 @@ cd ~
 # all the downloads in the universe
 curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
 curl -L -o godot.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_linux.x86_64.zip
-# curl -L -o templates.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_export_templates.tpz
+curl -L -o templates.zip https://github.com/godotengine/godot/releases/download/4.1.1-stable/Godot_v4.1.1-stable_export_templates.tpz
 
 # set up butler
 unzip butler.zip
@@ -15,6 +15,6 @@ unzip godot.zip
 mv Godot_v4.1.1-stable_linux.x86_64 godot
 
 # set up templates
-# mkdir -p ~/.local/share/godot/export_templates
-# unzip templates.zip
-# mv templates ~/.local/share/godot/export_templates/4.1.1.stable
+mkdir -p ~/.local/share/godot/export_templates
+unzip templates.zip
+mv templates ~/.local/share/godot/export_templates/4.1.1.stable
