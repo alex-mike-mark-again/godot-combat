924fc36d95d7569d283a6657b3a774ef85531682
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2023-09-20 21:28:25 -0500

    Add display to PC. Remove PC script from main scene. what the heck was that doing there?

--- player.gd
+++ player.gd
@@ -1,6 +1,17 @@
 extends Battler
 
 var def
+@export var stats: Label
 
 func _ready():
 	def = 1
+	hp = mhp # i don't wanna be calling this everytime!!!
+	# how do we get this to set properly in Battler?
+	
+	_updateDisplays()
+
+func deal_damage(damage: int):
+	hp = max(hp-damage,0)
+	
+func _updateDisplays():
+	stats.text = "❤️ "+str(hp)+"/"+str(mhp)+"\n🗡️ "+str(atk)
