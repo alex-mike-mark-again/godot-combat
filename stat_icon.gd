38999ab3b40a8b674d50aa93bb181f3ec743e253
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2023-10-19 21:18:58 -0500

    Stat increases glow green.

--- stat_icon.gd
+++ stat_icon.gd
@@ -1,16 +1,13 @@
 extends TextureRect
 
-# needs to uh. God.
-# like, make a _new_ delta label.
-# then play the animation on it.
-# instead of this.
-# also, not be red for positive changes.
-
 # also use colours from our pallete
 func play_delta_anim(delta: int):
 	var x = $current_label/delta_label.duplicate()
 	$current_label.add_child(x)
 	x.text = str(delta);
+	if(delta > 0):
+		x.add_theme_color_override("font_color", Color.FOREST_GREEN)
+		
 	x.get_node("AnimationPlayer").play("delta")
 	await x.get_node("AnimationPlayer").animation_finished
 	x.free()
