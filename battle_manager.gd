0d1e2943122c03e2432afb2bffc7bd33a2077d8a
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2024-02-13 12:56:14 -0600

    Removes globals

--- battle_manager.gd
+++ battle_manager.gd
@@ -4,7 +4,6 @@ class_name BattleManager
 var selected = null
 var enemies
 var prevPlayerStats
-var globals
 var currentTroop
 var battleCount = 0
 var turnsTaken = 0
@@ -12,11 +11,9 @@ var clickOn = true
 @export var gameOver: Node
 @export var player: Node
 
-
 @export var troops = []
 
 func _ready():
-	globals = get_node("/root/Globals")
 	on_player_stage_win()
 	load_current_troop()
 	$next_button.hide()
@@ -24,11 +21,11 @@ func _ready():
 func on_select(e):
 	if !clickOn:
 		return
-		
+
 	if selected != null && selected.name == e.name:
 		await _battle()
 		_check_end_of_battle()
-	
+
 	selected = e
 
 func _battle():
@@ -38,24 +35,22 @@ func _battle():
 	selected.reduce_atk(player.rdc)
 	await selected.animationPlayer.animation_finished
 	selected = null # works, but doesn't like. un activate the button.
-	
+
 	for enemy in enemies:
 		if !enemy.dead && enemy.atk:
 			enemy.attack_vfx()
 			await enemy.animationPlayer.animation_finished
 			player.take_damage(enemy.atk)
-	
-	clickOn = true	
-	
+
+	clickOn = true
+
 func _check_end_of_battle():
 	if player.dead:
 		gameOver.show()
-		
+
 	if _player_win():
 		player.on_victory()
 		if !on_player_stage_win(): #this is kinda weird looking
-			globals.finalTurns = turnsTaken
-			globals.finalLight = player.hp
 			get_tree().change_scene_to_file("res://game_won.tscn")
 
 func _player_win():
@@ -66,20 +61,25 @@ func _all_enemies_killed():
 	for enemy in enemies:
 		if !enemy.dead:
 			r = false
-	
+
 	return r
 
 func on_enemy_killed(e):
 	player.apply_buff(e.buff)
 
+# TODO: Redo this end of stage stuff again.
+# on_player_stage_win should just. display the next button
+# get the player stats, display the troop's victory screen.
+# advance_to_next_stage SHOULD be what's called on the button press.
+# and that should unload the troop, load the new one, etc.
 func on_player_stage_win():
 	prevPlayerStats = player.get_stats()
 	$next_button.show()
-	
+
 	if currentTroop:
 		remove_child(currentTroop)
 		currentTroop.queue_free()
-		
+
 	if battleCount < troops.size():
 		# THIS chunk is what loads the next battle.
 		currentTroop = load(troops[battleCount]).instantiate()
@@ -87,7 +87,7 @@ func on_player_stage_win():
 		return true
 	else:
 		return false
-	
+
 func load_current_troop():
 	add_child(currentTroop)
 	move_child(currentTroop,0)
@@ -95,7 +95,7 @@ func load_current_troop():
 	for enemy in enemies:
 		enemy.selected.connect(on_select)
 		enemy.died.connect(on_enemy_killed)
-		
+
 
 func retry():
 	# This should really all be in the player
--- globals.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var finalLight: int
-var finalTurns: int
-
-func _ready():
-	print("globals ready")
