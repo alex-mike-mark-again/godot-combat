c06352c9fa5d3c2f5931c7a2340d8646db477e3b
Alex Mike Mark <alex.mike.mark.again@gmail.com>
2024-03-02 12:27:13 -0600

    Aaaaaand it sorta works. Maybe.

--- ui/advancing_text_box.gd
+++ ui/advancing_text_box.gd
@@ -1,10 +1,11 @@
 extends Label
 @export var text_list: Array[String]
 var text_index = -1
+var end_text = "Escaped. Nextscaped.\nWe're over, hoser."
 
 func advance():
 	text_index = text_index + 1
 	if text_index < text_list.size():
-		text = text + text_list[text_index]
+		text = " ".join([text,text_list[text_index].c_unescape()])
 	else:
-		text = "donezo"
+		text = end_text
